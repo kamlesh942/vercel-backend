3d7453c3af272832a0de9468f523e4927b0865fc
kamlesh rajak <kamleshrajak888@gmail.com>
2025-11-02 17:43:31 +0530

    cor url

--- src/middlewares/cors.middleware.js
+++ src/middlewares/cors.middleware.js
@@ -1,7 +1,7 @@
 import cors from "cors";
 
 const corsMiddleware = cors({
-  origin: process.env.CORS_ORIGIN || "http://localhost:3000",
+  origin: process.env.CORS_ORIGIN || "https://vercel-frontend-lime.vercel.app",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: [

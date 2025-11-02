a046e523179fa3effcecd0e6faeaca5d28c2749c
kamlesh rajak <kamleshrajak888@gmail.com>
2025-11-02 17:31:15 +0530

    vercel-backend

---
+++ src/db/user.db.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URL}/grocery_website`
+      // `mongodb://localhost:27017/grocery_website`
+    );
+    console.log(
+      `\nMongoDB connected!! DB host: ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("MongoDB connection FAILED", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
